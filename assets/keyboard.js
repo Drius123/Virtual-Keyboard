b12598e6fded37d634eb18da7320e1d5a25d9f35
Андрей <adovgulev111@gmail.com>
2023-04-28 20:40:16 +0300

    init: add working files and some code in index.js and style.css

---
+++ assets/keyboard.js
@@ -0,0 +1,77 @@
+export default `
+
+<header class="header">
+<h1 class="main-article">Virtual Keyboard</h1>
+<textarea class="textarea"></textarea>
+</header>
+<main class="main">
+<section class="section-keyboard">
+  <div class="keyboard">
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+    <button class="btn-key" id=""></button>
+  </div>
+</section>
+</main>
+`;
---
+++ index.js
@@ -0,0 +1,29 @@
+/* eslint-disable import/extensions */
+import keyboard from './assets/keyboard.js';
+
+// Realize class Virtualkeyboard
+class Virtualkeyboard {
+  constructor() {
+    this.keyNumberBtn = ['`', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '-', '=', 'Backspace'];
+    this.keyFirstLineEng = ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\'];
+    this.keySecondLineEng = ['CapsLk', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '\'', 'Enter'];
+    this.keyThirdLineEng = ['Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', 'Shift', 'Up'];
+    this.keyFourthLineEng = ['Ctrl', 'win', 'Alt', 'Space', 'Alt', 'Ctrl', 'left', 'bottom', 'right'];
+    this.btn = document.createElement('button');
+    this.btn.classList.add('key-btn');
+  }
+
+  init() {
+    document.body.insertAdjacentHTML('afterbegin', keyboard);
+  }
+
+  addBtn() {
+    this.keyNumberBtn.forEach(item => document.body.children[1].append((this.btn)));
+  }
+}
+
+const virtKeyboard = new Virtualkeyboard();
+
+virtKeyboard.init();
+
+virtKeyboard.addBtn();
